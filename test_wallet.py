8ce18993bc37ff7c1f16b600f07d25a725df7531
Glykeria Stravodimou <strglykeria@gmail.com>
2021-04-20 13:09:41 +0100

    User can set their initial balance

--- test_wallet.py
+++ test_wallet.py
@@ -4,3 +4,7 @@ from wallet import Wallet
 def test_default_initial_amount():
   wallet = Wallet()
   assert wallet.balance == 0
+
+def test_setting_initial():
+  wallet = Wallet(10)
+  assert wallet.balance == 10
